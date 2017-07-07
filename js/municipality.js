f3778cdf5b9dc0018480a7d59df0f15ac538246a
Jonathan Beliën <jbe@geo6.be>
2017-07-07 14:26:10 +0200

    Split JavaScript in files

---
+++ js/municipality.js
@@ -0,0 +1,61 @@
+var $ = require("jquery");
+var ol = require("openlayers");
+
+var format = require("./format.js");
+
+/**
+ *
+ */
+exports.load = function (callback) {
+  var municipalitySource;
+  var municipalityLayer;
+
+  $.getJSON("./data/municipalities.json", function (json) {
+    municipalitySource = new ol.source.Vector({
+      features: (new ol.format.GeoJSON()).readFeatures(json, {
+        dataProjection: "EPSG:4326",
+        featureProjection: global.map.getView().getProjection()
+      })
+    });
+    /*
+    municipalityLayer = new ol.layer.Vector({
+      source: municipalitySource
+    });
+    municipalityLayer.setMap(global.map);
+    */
+
+    callback({
+      "layer": municipalityLayer,
+      "source": municipalitySource
+    });
+  });
+
+  return true;
+}
+
+/**
+ *
+ */
+exports.onChange = function () {
+  $("#geometry").val("");
+  $("#main-download").remove();
+
+  global.features.clear();
+  $("#btn-create-export").prop("disabled", true);
+
+  global.map.removeInteraction(global.interaction.draw);
+  global.map.removeInteraction(global.interaction.modify);
+
+  if ($("#municipality").val() !== "") {
+    var mun = global.municipality.source.getFeatureById($(this).val());
+
+    global.features.push(mun);
+    global.map.getView().fit(mun.getGeometry());
+
+    var output = format.formatArea(mun.getGeometry());
+
+    $("#main-infos-area").show().find("span").html(output);
+
+    $("#btn-create-export").prop("disabled", false);
+  }
+}
